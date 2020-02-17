baf7e2d31bdd9918ec0c92385610d9f2b91ccb4e
Bob <crowcoder@gmail.com>
2020-02-17 07:54:11 -0500

    initial repo creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
